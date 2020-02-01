af030815c9582f0dbef30254b76a85b6c6fef85b
Wesley Moses <wesmoses@gmail.com>
2020-02-01 23:32:24 +0200

    Updates

---
+++ precache-manifest.bca35ab469f8e7f56226881024ef5b4d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1144e728a4643bb12312792e1ff83d4",
+    "url": "/dpkg-status-htmlizer/index.html"
+  },
+  {
+    "revision": "5f1d1a2d5b15153b6a27",
+    "url": "/dpkg-status-htmlizer/static/css/2.d4fdcb4b.chunk.css"
+  },
+  {
+    "revision": "7a037332f9e678425c17",
+    "url": "/dpkg-status-htmlizer/static/css/main.907ab4bd.chunk.css"
+  },
+  {
+    "revision": "5f1d1a2d5b15153b6a27",
+    "url": "/dpkg-status-htmlizer/static/js/2.e6dcc67d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dpkg-status-htmlizer/static/js/2.e6dcc67d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a037332f9e678425c17",
+    "url": "/dpkg-status-htmlizer/static/js/main.423674d8.chunk.js"
+  },
+  {
+    "revision": "b1ffa975699daad438d4",
+    "url": "/dpkg-status-htmlizer/static/js/runtime-main.7db52e67.js"
+  },
+  {
+    "revision": "1b683111cb3b0b8e497307c2c723c0d5",
+    "url": "/dpkg-status-htmlizer/static/media/Fixedsys500c.1b683111.eot"
+  },
+  {
+    "revision": "2bf884c69a05089a20d443c1d2af2f95",
+    "url": "/dpkg-status-htmlizer/static/media/Fixedsys500c.2bf884c6.svg"
+  },
+  {
+    "revision": "8ad84d20444bf9679cc5739424e9550f",
+    "url": "/dpkg-status-htmlizer/static/media/Fixedsys500c.8ad84d20.ttf"
+  },
+  {
+    "revision": "c1fc2bed5c45c933f95903a6f0bddbac",
+    "url": "/dpkg-status-htmlizer/static/media/Fixedsys500c.c1fc2bed.woff"
+  },
+  {
+    "revision": "efe3c24329652fbea0442f8b2feaab86",
+    "url": "/dpkg-status-htmlizer/static/media/status.efe3c243.real"
+  }
+]);
